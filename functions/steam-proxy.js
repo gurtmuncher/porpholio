d8bacba4f9e4006e466f22615349083827f5a352
collinc <collinwade6173@gmail.com>
2026-02-24 19:16:58 -0600

    lp

--- functions/steam-proxy.js
+++ functions/steam-proxy.js
@@ -23,7 +23,7 @@ export async function onRequest(context) {
     let res = await fetch(recentUrl, {
         headers: { "User-Agent": "cf-pages-steam-proxy" }
     });
-//l
+//lp
     if (!res.ok) {
         const allUrl =
             "https://steamcommunity.com/profiles/" +
