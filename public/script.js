0bd1ffce31a2466d7db10630166d13b5a7e5d4ff
collinc <collinwade6173@gmail.com>
2026-03-16 22:21:36 -0500

    sfhihifsa

--- public/script.js
+++ public/script.js
@@ -17,15 +17,17 @@ var vantaEffect = null;
 var discordId = '1321090812947468288';
 
 var songs = [
-    { src: 'audio/brave_shine.mp3', title: 'Brave Shine - Aimer' },
-    { src: 'audio/rising_hope.mp3', title: 'Rising Hope - LiSA' },
-    { src: 'audio/unravel.mp3', title: 'Unravel - TK' },
-    { src: 'audio/like_flames.mp3', title: 'Like Flames - MindaRyn' },
-    { src: 'audio/song.mp3', title: '青のすみか - キタニタツヤ' },
-    { src: 'audio/numb.mp3', title: 'Comfortably Numb Solo Cover - Thomas75s' },
-    { src: 'audio/luhcalm.mp3', title: 'ふたりの気持ち' },
-    { src: 'audio/aot-la-la-la.mp3', title: 'Attack on titan la la la' },
-    { src: 'audio/spend-some-goon-away.mp3', title: 'Chamber Of Reflection - Mac DeMarco' }
+    { src: '/audio/brave_shine.mp3', title: 'Brave Shine - Aimer' },
+    { src: '/audio/rising_hope.mp3', title: 'Rising Hope - LiSA' },
+    { src: '/audio/unravel.mp3', title: 'Unravel - TK' },
+    { src: '/audio/like_flames.mp3', title: 'Like Flames - MindaRyn' },
+    { src: '/audio/song.mp3', title: '青のすみか - キタニタツヤ' },
+    { src: '/audio/numb.mp3', title: 'Comfortably Numb Solo Cover - Thomas75s' },
+    { src: '/audio/luhcalm.mp3', title: 'ふたりの気持ち' },
+    { src: '/audio/aot-la-la-la.mp3', title: 'Attack on titan la la la' },
+    { src: '/audio/spend-some-goon-away.mp3', title: 'Chamber Of Reflection - Mac DeMarco' },
+    { src: '/audio/relax.mp3', title: 'Relax - Vacations' },
+    { src: '/audio/home.mp3', title: 'Home - Vacations' }
 
 ];
 
